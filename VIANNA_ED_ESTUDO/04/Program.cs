f8684e18550989d97c77b4088d43fae5cf0045ff
JuanPabloGarciaManoel <79871430+JuanPabloGarciaManoel@users.noreply.github.com>
2024-03-02 03:44:52 +0000

    Estudo

---
+++ VIANNA_ED_ESTUDO/04/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    public static int MDC_RECURSIVIDADE(int a, int b){ /*A>B*/
+        if(b == 0)
+            return a;
+        else{
+            Console.WriteLine($"Chamando MDC_RECURSIVIDADE({b}, {a % b})");
+            return MDC_RECURSIVIDADE(b,a % b);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine(MDC_RECURSIVIDADE(48,18));
+        Console.WriteLine(7%3);
+        Console.WriteLine(20%15);
+    }
+}
