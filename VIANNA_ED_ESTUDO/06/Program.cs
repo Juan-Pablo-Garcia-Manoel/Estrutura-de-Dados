b01b5d590afe36fd7d55e163cf80b15be0ab62a5
JuanPabloGarciaManoel <juan.manoel@viannasempre.com.br>
2024-03-18 18:04:12 -0300

    Estudo

--- VIANNA_ED_ESTUDO/06/Program.cs
+++ VIANNA_ED_ESTUDO/06/Program.cs
@@ -8,6 +8,7 @@
     OrdenarPorSelecao(vet);
     
     Imprimir(vet);
+    Console.ReadKey();
 
     }
 
---
+++ VIANNA_ED_ESTUDO/07/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    public static int multiplicacao(int a, int b){
+        if(b == 0)
+            return 0;
+        else
+            return a + multiplicacao(a,b - 1);
+    }
+
+    public static int fatorial(int a){
+        if(a == 0)
+            return 1;
+        else
+            return a * fatorial(a - 1);
+    }
+
+    public static void imprimir(string mensagem, int numeroVezes){
+        Console.WriteLine($"Empilhou. Valor parâmetro: {numeroVezes}");
+        if(numeroVezes > 0){
+            imprimir(mensagem, numeroVezes - 1);
+            Console.WriteLine($"{numeroVezes}) {mensagem}");
+        }
+        Console.WriteLine($"Desempilhou. Valor parâmetro: {numeroVezes}");
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(multiplicacao(2,4));
+        Console.WriteLine(fatorial(4));
+        imprimir("Olá mundo", 5);
+    }
+}
