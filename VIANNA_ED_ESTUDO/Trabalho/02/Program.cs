5f20d6c9876dfacb280be0ec834a00b0cfe70fa4
JuanPabloGarciaManoel <79871430+JuanPabloGarciaManoel@users.noreply.github.com>
2024-03-27 00:57:14 +0000

    Trabalho

--- VIANNA_ED_ESTUDO/Trabalho/02/Program.cs
+++ VIANNA_ED_ESTUDO/Trabalho/02/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program{
+    
+    /*
+    Faça uma função que receba um vetor de números inteiros por parâmetro e multiplique
+    por -1 todos os elementos negativos desse vetor. A função deve retornar o número de
+    elementos negativos encontrados. Restrição: não utilize comandos de repetição.
+    */
+
+    public static int funcaoVetor(int[] vetorInteiro){
+        int pos = vetorInteiro.Length;
+        
+        if(pos > 0){
+            funcaoVetor(pos - 1);
+            Console.WriteLine("{0}", vetorInteiro[pos]);
+        }
+
+        return vetorInteiro[pos];
+    }
+ 
+    public static void Main(string[] args){
+        
+        int[] vetorInteiro = new int[]{0,2,-5,3,-7};
+        int resultado;
+
+        resultado = funcaoVetor(vetorInteiro);
+        Console.WriteLine("{0}",resultado);
+
+    }
+
+}
