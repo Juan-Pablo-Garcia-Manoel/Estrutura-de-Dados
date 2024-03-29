0948e5674fd455e3a06815001072031285e3a39d
JuanPabloGarciaManoel <juan.manoel@viannasempre.com.br>
2024-03-29 17:28:02 -0300

    Trabalho

--- VIANNA_ED_ESTUDO/Trabalho/02/Program.cs
+++ VIANNA_ED_ESTUDO/Trabalho/02/Program.cs
@@ -7,27 +7,46 @@
     */
 
     public static int funcaoVetor(int[] vetorInteiro){
+        return funcaoVetor(vetorInteiro,0);
+    }
+
+    public static int funcaoVetor(int[] vetorInteiro, int posicao){
         
-        int posicao = 0;
-        int aux = vetorInteiro[0];
-        
+        int qtdNegativos = 0;
+
         if(posicao < vetorInteiro.Length){
-            posicao++;
-            aux = vetorInteiro[posicao];
-            funcaoVetor(aux);
+            
+            if(vetorInteiro[posicao] < 0){
+                vetorInteiro[posicao] *= -1;
+                qtdNegativos++;
+            }
+
+            qtdNegativos += funcaoVetor(vetorInteiro, posicao + 1);
         }
 
-        return aux;
+        return qtdNegativos;
 
+    }
+
+    static void Imprimir(int[] vetorInteiro, int posicao = 0)
+    {
+        if (posicao < vetorInteiro.Length)
+        {
+            Console.Write("{0} ", vetorInteiro[posicao]);
+            Imprimir(vetorInteiro, posicao + 1);
+        }
     }
  
     public static void Main(string[] args){
         
-        int[] vetorInteiro = new int[]{0,2,-5,3,-7};
         int resultado;
 
+        int[] vetorInteiro = new int[]{0,2,-5,3,-7,-8};
+        
         resultado = funcaoVetor(vetorInteiro);
-        Console.WriteLine("{0}",resultado);
+        Console.WriteLine("Quantidade valores negativos encontrados: {0}",resultado);
+
+        Imprimir(vetorInteiro);
 
     }
 
