6a4a9ff3b1374ea8804f9ecc8b88c7c9e43366f9
JuanPabloGarciaManoel <79871430+JuanPabloGarciaManoel@users.noreply.github.com>
2024-05-16 00:21:31 +0000

    Ajuste

--- VIANNA_ED_AULA/02/Program.cs
+++ VIANNA_ED_AULA/02/Program.cs
@@ -8,10 +8,21 @@ class Program
 
         lista.AdicionarNoInicio(10);
         lista.AdicionarNoInicio(7);
-        lista.AdicionarNoInicio(2);
-        lista.AdicionarNoFinal(50);
-        lista.AdicionarNoFinal(100);
+        lista.AdicionarNoInicio(2); //2 7 10
+        lista.AdicionarNoFinal(50); //2 7 10 50 1
+        lista.AdicionarNoFinal(1);
 
+        lista.RemoverNoInicio(); //7 10 50 1
+        lista.RemoverNoFinal();//7 10 50
+
+        lista.ParaCada(x => Console.Write($"{x} "));
+        Console.WriteLine();
+
+        int soma = 0;
+        lista.ParaCada(x => soma += x);
+        Console.WriteLine($"Soma: {soma}");
+
+        /*
         var noAtual = lista.Primeiro;
 
         while (noAtual != null)
@@ -19,5 +30,6 @@ class Program
             Console.Write($"{noAtual.Valor}  ");
             noAtual = noAtual.Proximo;
         }
+        */
     }
 }
